fc60c3d2e1ac9e30f05da60d7e6ee75aef53d604
azmi <nurazmiprasetyo@gmail.com>
2025-05-11 15:13:38 +0700

    New update on API security

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hogxyjhgngotccaldjbm.supabase.co'; // replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvZ3h5amhnbmdvdGNjYWxkamJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzOTI3MzgsImV4cCI6MjA2MTk2ODczOH0.Hlt2e_Q8j2xW_ox43U7kMVlOiKWdufwDCTVJ4tE-pIk'; // replace this
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
