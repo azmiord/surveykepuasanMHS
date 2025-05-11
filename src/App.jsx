d980954868ff89e1af860a50b90846608faeb470
azmi <nurazmiprasetyo@gmail.com>
2025-05-11 15:11:37 +0700

    New upadate on API security

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import NavigationButtons from './components/NavigationButtons';
 import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = 'https://hogxyjhgngotccaldjbm.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvZ3h5amhnbmdvdGNjYWxkamJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzOTI3MzgsImV4cCI6MjA2MTk2ODczOH0.Hlt2e_Q8j2xW_ox43U7kMVlOiKWdufwDCTVJ4tE-pIk';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const App = () => {
